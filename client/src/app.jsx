6d78fe4264c6d6e07e6ceede3cd61882cd7bfe6b
Logan Michalicek <logan.michalicek@gmail.com>
2021-09-07 07:34:46 -0600

    Added react router example to app.jsx

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import AppCSSLight from './appLight.module.css';
 import AppCSSDark from './appDark.module.css';
 
-function App() {
-  return (
-    <div id="App">
-      <h1 className={AppCSSLight.testBanner}>I'm loaded from the Light Mode style sheet!</h1>
-      <h1 className={AppCSSDark.testBanner}>And I'm loaded from the Dark Mode style sheet!</h1>
+import ProductOverview from './components/productOverview/productOverview';
 
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      productIds: ['001', '002', '003'] // temporary to test react router
+    };
+  }
+
+  render() {
+    return (
+      <div id="App">
+        <Router>
+          <div>
+            <ul>Links
+              <li>
+                <Link to='Home'>Home</Link>
+                <Link to='/001'>001</Link>
+              </li>
+            </ul>
+            <Switch>
+              <Route exact path='/'>
+                <h1>Home</h1>
+              </Route>
+              <Route path='/001'>
+                <h3>001 Page</h3>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+        <h1 className={AppCSSLight.testBanner}>I'm loaded from the Light Mode style sheet!</h1>
+        <h1 className={AppCSSDark.testBanner}>And I'm loaded from the Dark Mode style sheet!</h1>
+
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
---
+++ client/src/components/productOverview/productOverview.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ProductOverview extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <h1>Product Overview</h1>
+    );
+  }
+}
+
+export default ProductOverview;
