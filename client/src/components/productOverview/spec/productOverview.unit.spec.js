cfb3c04b908d08ebad8d2ded1e440baed9aa769b
Logan Michalicek <logan.michalicek@gmail.com>
2021-09-08 20:48:02 -0600

    Further work on skeleton

---
+++ client/src/components/productOverview/spec/productOverview.unit.spec.js
@@ -0,0 +1,5 @@
+describe("Product Overview", function() {
+  it("Should run without issue", function() {
+    expect(1).toEqual(1);
+  })
+});
--- client/src/components/productOverview/subcomponents/ProductInfo.jsx
+++ client/src/components/productOverview/subcomponents/ProductInfo.jsx
@@ -12,6 +12,24 @@ const ProductInfo = ( props ) => {
       <h3>$749</h3>
       <h3>STYLE > SELECTED STYLE</h3>
       <StyleThumbnailGrid styles={props.styles} />
+      <div id='dropdown-row'>
+        <select defaultValue="default">
+          <option value="default" disabled hidden>Choose size</option>
+          <option>S</option>
+          <option>M</option>
+          <option>L</option>
+        </select>
+        <select defaultValue="default">
+          <option value="default" disabled hidden>Choose quantity</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+        </select>
+      </div>
+      <div id='btn-row'>
+        <button>Add to Bag</button>
+        <button>[STAR]</button>
+      </div>
     </div>
   );
 }
