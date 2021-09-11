32e4796ee4ae0176456b4650b71cfdebf015ed20
Logan Michalicek <logan.michalicek@gmail.com>
2021-09-11 14:41:25 -0600

    Fixed merging issues

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -13,7 +13,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       productInfo: {},
-      styleInfo: {}
+      styleInfo: {},
       darkmode: false,
     };
     this.darkmodeToggle = this.darkmodeToggle.bind(this);
