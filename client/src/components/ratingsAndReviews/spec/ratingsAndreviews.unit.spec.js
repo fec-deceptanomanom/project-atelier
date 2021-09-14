723c559730541fb6c93033340992d9c5e25be2fe
twicecooked <ryanshaorou@gmail.com>
2021-09-14 00:07:24 -0400

    Basic tests for component

--- client/src/components/ratingsAndReviews/spec/ratingsAndreviews.unit.spec.js
+++ client/src/components/ratingsAndReviews/spec/ratingsAndreviews.unit.spec.js
@@ -4,6 +4,9 @@ import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 import ReviewList from '../subComponents/reviewList.jsx'
+import SizeComfortScale from '../subComponents/sizeComfortScale.jsx'
+import StarRatings from  '../subComponents/starRatings.jsx';
+import Reviews from  '../subComponents/reviews.jsx'
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -13,10 +16,34 @@ describe("Product Overview", function() {
   })
 });
 
-describe('ReviewList', () => {
+describe('ReviewList Inclusion', () => {
   it('renders without crashing and contains X element', () => {
     const wrapper = Enzyme.shallow(<ReviewList />);
     const tester = <h4>xyz reviews, sort by relevance</h4>;
+    expect(wrapper.contains(tester)).toEqual(true);
+  });
+});
+
+describe('SizeComfortScale Inclusion', () => {
+  it('renders without crashing and contains X element', () => {
+    const wrapper = Enzyme.shallow(<SizeComfortScale />);
+    const tester = <div>Size Scale</div>;
+    expect(wrapper.contains(tester)).toEqual(true);
+  });
+});
+
+describe('StarRatings Inclusion', () => {
+  it('renders without crashing and contains X element', () => {
+    const wrapper = Enzyme.shallow(<StarRatings />);
+    const tester = <div>Star Ratings Here</div>
+    expect(wrapper.contains(tester)).toEqual(true);
+  });
+});
+
+describe('Reviews Inclusion', () => {
+  it('renders without crashing and contains X element', () => {
+    const wrapper = Enzyme.shallow(<Reviews />);
+    const tester = <div>This is a review.</div>;
     expect(wrapper.contains(tester)).toEqual(true);
   });
 });
