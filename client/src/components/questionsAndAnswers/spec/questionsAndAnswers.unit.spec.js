637fd17a8415f14feb7d2bb8c715b4bb6ddbb04f
Sarah Cerezo <cerezo.sarah@gmail.com>
2021-09-17 19:49:49 -0400

    Some attempts at more robust tests; WIP

--- client/src/components/questionsAndAnswers/spec/questionsAndAnswers.unit.spec.js
+++ client/src/components/questionsAndAnswers/spec/questionsAndAnswers.unit.spec.js
@@ -1,6 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import QuestionsAndAnswers from '../QuestionsAndAnswers';
+import SearchBar from '../subcomponents/SearchBar';
+
 // initial test just for display purposes
-describe("Questions and Answers", function() {
-  it("Test suite is running", function() {
+describe("Test suite set up", function() {
+  it("proves the test suite is running", function() {
     expect(1).toEqual(1);
   })
+});
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+// Initial component tests
+
+describe('Main COmponent Renders', () => {
+  it('renders without crashing and contains X element', () => {
+    const wrapper = Enzyme.render(<QuestionsAndAnswers />);
+    expect(wrapper.find('.QuestionsAndAnswers')).to.have.lengthOf(1);
+  });
 });
