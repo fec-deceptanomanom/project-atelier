dba8efa48138ce93a64f01997d01839659cf2d90
Sarah Cerezo <cerezo.sarah@gmail.com>
2021-09-18 11:49:35 -0400

    Updated Q&A spec with some basic component tests

--- client/src/components/questionsAndAnswers/spec/questionsAndAnswers.unit.spec.js
+++ client/src/components/questionsAndAnswers/spec/questionsAndAnswers.unit.spec.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-import QuestionsAndAnswers from '../QuestionsAndAnswers';
+import AnswerEntry from '../subcomponents/AnswerEntry';
+import AnswersList from '../subcomponents/AnswersList';
+import QuestionEntry from '../subcomponents/QuestionEntry';
+import QuestionsList from '../subcomponents/QuestionsList';
 import SearchBar from '../subcomponents/SearchBar';
+
+import testData from './testData';
+import CSSStyle from '../styles/QandALight.module.css';
 
 // initial test just for display purposes
 describe("Test suite set up", function() {
@@ -18,9 +24,26 @@ Enzyme.configure({ adapter: new Adapter() });
 
 // Initial component tests
 
-describe('Main COmponent Renders', () => {
-  it('renders without crashing and contains X element', () => {
-    const wrapper = Enzyme.render(<QuestionsAndAnswers />);
-    expect(wrapper.find('.QuestionsAndAnswers')).to.have.lengthOf(1);
+describe('Answers Entry', () => {
+  it('renders without crashing', () => {
+    const wrapper = Enzyme.shallow(<AnswerEntry CSSStyle={CSSStyle} answerData={testData.results[0]["answers"][68]}/>);
+    const element = <h3>A: </h3>
+    expect(wrapper.contains(element)).toEqual(true);
+  });
+});
+
+describe('Question Entry', () => {
+  it('renders without crashing', () => {
+    const wrapper = Enzyme.shallow(<QuestionEntry key={0} CSSStyle={CSSStyle} questionData={testData.results[0]} openAnswerForm={() => {return false}}/>);
+    const element = <h3 id="question-37">Q: Why is this product cheaper here than other sites?</h3>
+    expect(wrapper.contains(element)).toEqual(true);
+  });
+});
+
+describe('SearchBar', () => {
+  it('renders without crashing', () => {
+    const wrapper = Enzyme.shallow(<SearchBar CSSStyle={CSSStyle} />);
+    const element = <button type="submit" className={CSSStyle.searchButton}><i className="fas fa-search"></i></button>
+    expect(wrapper.contains(element)).toEqual(true);
   });
 });
