4f7dfff48e171e0bba0257950894f474197076f8
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-09-17 21:09:33 -0400

    Revert "init branch"
    This reverts commit de05544569d84664c7afdf4004fda1aea5a60671.

--- client/src/components/relatedItems/subcomponents/RelatedCard.jsx
+++ client/src/components/relatedItems/subcomponents/RelatedCard.jsx
@@ -4,47 +4,29 @@ import CSSDark from '.././relatedItemsDark.module.css';
 
 import ComparisonModal from './ComparisonModal.jsx';
 
-class RelatedCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const RelatedCard = (props) => {
 
-    }
-  }
+  const starButton = (
+    <div>
+      <button onClick={() => {}} value='Star Button'>Star Button</button>
+    </div>
+  );
 
-  render() {
-    const starButton = (
-      <div>
-        <button onClick={() => {}} value='Star Button'>Star Button</button>
-      </div>
-    );
-    let thumbnail = 'Loading';
+  return (
+    <div className={CSSLight.relatedCard}>
+      <h2>I am a Related Product Card</h2>
+      {starButton}
+      <h3>Preview Image</h3>
 
-
-    return(
-      <div className={CSSLight.relatedCard}>
-        <h2>Loading</h2>
-        {starButton}
-        <img src={thumbnail} alt='Loading' width='50'></img>
-
-        <ul>
-          <li>Product Category</li>
-          <li>Product Name</li>
-          <li>Price</li>
-          <li>Star Rating</li>
-        </ul>
-      </div>
-    )
-
-  }
+      <ul>
+        <li>Product Category</li>
+        <li>Product Name</li>
+        <li>Price</li>
+        <li>Star Rating</li>
+      </ul>
+    </div>
+  )
 }
-  // if (this.props.cardInfo.styleInfo.results[0].photos[0].thumbnail_url) {
-  //   thumbnail = this.props.cardInfo.styleInfo.results[0].photos[0].thumbnail_url;
-  // }
-
-// if props.carInfo exists do b
-
-// else depict message saying not founded
 
 
 export default RelatedCard;
