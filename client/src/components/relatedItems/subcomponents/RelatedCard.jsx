1741b04e249e60535a9e81d51fb86421d46bd91c
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-09-18 21:07:10 -0400

    API coming down through. No more bugs when default images cannot be found.

--- client/src/components/relatedItems/subcomponents/RelatedCard.jsx
+++ client/src/components/relatedItems/subcomponents/RelatedCard.jsx
@@ -8,13 +8,36 @@ class RelatedCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      photo: 'Loading...',
+      crutches: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn15.bestreviews.com%2Fimages%2Fv4desktop%2Fproduct-matrix%2Fdmi-crutches--push-button-adjustable-crutches-f94027.jpg&f=1&nofb=1'
 
     }
   }
 
-  findDefaultPhoto(results) {
-    let defal = results.find(result => result['default?']);
-    return defal.photos[0].thumbnail_url
+  findDefaultResult(results) {
+    //Find the default object
+    let defal = results.find(result => result['default?']) || 'Not Found';
+    return defal;
+  }
+
+  findThumbnail(item) {
+    //select the first thumbnail found or give'em some crutches
+    if (item === 'Not Found') { return this.state.crutches; }
+
+    let photo = item.photos.find( photo => photo.thumbnail_url);
+
+    if (!photo) { return this.state.crutches; }
+
+    return photo.thumbnail_url;
+  }
+
+  componentDidMount() {
+    const defaultItem = this.findDefaultResult(this.props.cardInfo.styleInfo.results);
+    const thumbnail = this.findThumbnail(defaultItem);
+
+    this.setState({
+      photo: thumbnail
+    })
   }
 
   render() {
@@ -24,14 +47,13 @@ class RelatedCard extends React.Component {
       </div>
     );
     const info = this.props.cardInfo.productInfo;
-    const thumbnail = this.findDefaultPhoto(this.props.cardInfo.styleInfo.results);
 
     return (
       <div className={CSSLight.relatedCard}>
         <h2>{info.name}</h2>
         <h4>{info.category}</h4>
         {starButton}
-        <img className={CSSLight.thumbnail} src={thumbnail} alt='Image not Found'></img>
+        <img className={CSSLight.thumbnail} src={this.state.photo} alt='Image not Found'></img>
         <ul>
           <li>{info.default_price}</li>
           <li>Star Rating</li>
