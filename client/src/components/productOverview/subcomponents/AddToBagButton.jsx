41e90a22812cec9dccbdc47013915c7668ab6adc
Logan Michalicek <logan.michalicek@gmail.com>
2021-09-22 22:37:17 -0600

    Added basic button functionality that ensures size selection

--- client/src/components/productOverview/subcomponents/AddToBagButton.jsx
+++ client/src/components/productOverview/subcomponents/AddToBagButton.jsx
@@ -3,10 +3,21 @@ import CSSCommon from '../styles/productOverview.module.css';
 import CSSLight from '../styles/productOverviewLight.module.css';
 import CSSDark from '../styles/productOverviewDark.module.css';
 
+const $ = require('jquery');
+
+const onAddClick = () => {
+  let size = $('#product-size-selection').val();
+  if (size === null) {
+    alert('Please select a size');
+  } else {
+    console.log(`Adding size ${size} to bag!`)
+  }
+}
+
 const AddToBag = (props) => {
   if (!props.outOfStock) {
     return (
-      <button>Add To Bag</button>
+      <button onClick={onAddClick}>Add To Bag</button>
     );
   } else {
     return (
