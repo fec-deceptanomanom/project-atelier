4706b1e474ec162c8a286c2150fad6bf240d508d
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-09-23 23:13:29 -0400

    Modal is 'pulling up'.

--- client/src/components/relatedItems/subcomponents/ComparisonModal.jsx
+++ client/src/components/relatedItems/subcomponents/ComparisonModal.jsx
@@ -20,47 +20,57 @@ const ComparisonModal = (props) => {
     category: props.clickedItem.product.category,
     price: props.clickedItem.product.default_price,
     clickedRating
-
   };
+
   return (
     <div className={style.modal}>
       <div className={style.modalContent}>
         <i className="fa fa-times-circle fa=lg"
-          //  className={style.close}
-           aria-hidden="true"
-           onClick={props.toggleModal}></i>
-        <table>
-          <tbody>
-            <tr>
-              <th>Current Product</th>
-              <th>Characteristic</th>
-              <th>Compared Product</th>
+          aria-hidden="true"
+          onClick={props.toggleModal}>
+        </i>
+        <div>
+          <h2>Comparison</h2>
+          <table className={style.comparison}>
+            <colgroup>
+              <col></col>
+              <col span="1" className="characteristic"></col>
+              <col></col>
+            </colgroup>
+            <thead>
+            <tr className={style.labels}>
+              <th scope="col">Current Product</th>
+              <th scope="col">Characteristic</th>
+              <th scope="col">Compared Product</th>
             </tr>
-            <tr>
-              <td>{current.name}</td>
-              <td>Name</td>
-              <td>{clicked.name}</td>
-            </tr>
-            <tr>
-              <td>{current.category}</td>
-              <td>Dept</td>
-              <td>{clicked.category}</td>
-            </tr>
-            <tr>
-              <td>{current.price}</td>
-              <td>Price</td>
-              <td>{clicked.price}</td>
-            </tr>
-            <tr>
-              <td>{currentRating}</td>
-              <td>Rating</td>
-              <td>{clickedRating}</td>
-            </tr>
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{current.name}</td>
+                <th scope="row">Name</th>
+                <td>{clicked.name}</td>
+              </tr>
+              <tr>
+                <td>{current.category}</td>
+                <th scope="row">Dept</th>
+                <td>{clicked.category}</td>
+              </tr>
+              <tr>
+                <td>${current.price}</td>
+                <th scope="row">Price</th>
+                <td>${clicked.price}</td>
+              </tr>
+              <tr>
+                <td>{currentRating}</td>
+                <th scope="row">Rating</th>
+                <td>{clickedRating}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   )
 }
 
-export default ComparisonModal;
+          export default ComparisonModal;
