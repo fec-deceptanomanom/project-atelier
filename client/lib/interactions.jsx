36737667d7e2937157e556ec6bd6f5f45249ed7a
Sarah Cerezo <cerezo.sarah@gmail.com>
2021-09-29 23:19:05 -0400

    Tracked down all the in-component references to localhost as well

--- client/lib/interactions.jsx
+++ client/lib/interactions.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import $ from 'jquery';
+import { URL_BASE } from '../../.secret.json'
 
 export function postInteraction(element, widget) {
   // console.log(`${element} clicked in ${widget} component at ${Date.now()}!`);
   const time = String(Date.now());
   $.post({
-    url: 'http://localhost:3000/interactions',
+    url: `${URL_BASE}/interactions`,
     data: {element, widget, time},
     success: ( (data, status) => {
       //console.log('POST INTERACTIONS SUCCESS\n', data);
