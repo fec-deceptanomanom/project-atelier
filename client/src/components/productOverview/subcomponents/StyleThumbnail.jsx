1ad1343817db3ee03cd2363b4d75c3adca4fe93d
Logan Michalicek <logan.michalicek@gmail.com>
2021-10-05 17:31:44 -0600

    Added basic current style checkmark

--- client/src/components/productOverview/subcomponents/StyleThumbnail.jsx
+++ client/src/components/productOverview/subcomponents/StyleThumbnail.jsx
@@ -8,13 +8,16 @@ import { withClickTracker } from '../../../../lib/interactions.jsx';
 const StyleThumbnail = ( props ) => {
   const id = `style-thumbnail-${props.idx}`;
   return (
-    <button
-      id={id}
-      className={CSSCommon['style-thumbnail']}
-      onClick={() => {props.onStyleClick(props.style)}}
-    >
-      {props.style.name}
-    </button>
+    <div className={CSSCommon['style-thumbnail-wrapper']}>
+      <button
+        id={id}
+        className={CSSCommon['style-thumbnail']}
+        onClick={() => {props.onStyleClick(props.style)}}
+      >
+        {props.style.name}
+      </button>
+      <i id={CSSCommon['current-style-checkmark']} class="fas fa-check-circle"></i>
+    </div>
   );
 }
 
