d4a9e2442e26c0623338b5e3cae60c93cf17f92c
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-10-05 00:04:20 -0400

    end of night

--- client/src/components/relatedItems/subcomponents/ComparisonModal.jsx
+++ client/src/components/relatedItems/subcomponents/ComparisonModal.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import style from './ComparisonModal.module.css';
-
+// need to go off of product/features
+//fabric cut lenses uv protection, frames
+//if feature is null replace with a checkmark
 const ComparisonModal = (props) => {
-  const clickedStars = props.clickedItem.reviews.ratings;
-  const currentStars = props.currentItem.reviews.ratings;
-  const clickedRating = props.stars(clickedStars);
-  const currentRating = props.stars(currentStars);
 
-  const current = {
-    name: props.currentItem.product.name,
-    category: props.currentItem.product.category,
-    price: props.currentItem.product.default_price,
-    currentRating
-  };
+  const currentFeatures = props.currentItem.product.features;
+  const clickedFeatures = props.clickedItem.product.features;
+  const features = currentFeatures.concat(clickedFeatures);
+  console.log(features)
+  const numOfRows = features.length;
 
-  const clicked = {
-    name: props.clickedItem.product.name,
-    category: props.clickedItem.product.category,
-    price: props.clickedItem.product.default_price,
-    clickedRating
-  };
+  //iterate over features and build rows
+  const tableBody = (
+    <tbody id='comparison-modal-table-body'>{rows}</tbody>
+  )
 
+  const rows = featurs.map( (feat, i) => {
+    let trId = 'comparison-modal-table-row-' + {i};
+    let tdId = 'comparison-modal-table-td1' + {i};
+    let th1Id = 'comparison-modal-table-th-' + {i};
+    let td2Id = 'comparison-modal-table-td2-' + {i};
+    return (<tr id={trId}>
+              <td id={th1Id}>{i}</td>
+              <th id={tdId} scope="row">{feat}</th>
+              <td id={th2Id}>{i}</td>
+            </tr>);
+  })
   return (
     <div id='comparison-modal' className={style.modal}>
       <div id='comparison-modal-content' className={style.modalContent}>
@@ -38,33 +44,12 @@ const ComparisonModal = (props) => {
             </colgroup>
             <thead id='comparison-modal-table-head'>
             <tr id='comparison-modal-table-row-labels' className={style.labels}>
-              <th id='comparison-modal-table-th-current' scope="col">Current Product</th>
+              <th id='comparison-modal-table-th-current' scope="col">{props.currentItem.product.name}</th>
               <th id='comparison-modal-table-th-chars' scope="col">Characteristic</th>
-              <th id='comparison-modal-table-th-compare' scope="col">Compared Product</th>
+              <th id='comparison-modal-table-th-compare' scope="col">{props.clickedItem.product.name}</th>
             </tr>
             </thead>
-            <tbody id='comparison-modal-table-body'>
-              <tr id='comparison-modal-table-row-name'>
-                <td id='comparison-modal-table-td1name'>{current.name}</td>
-                <th id='comparison-modal-table-th-name' scope="row">Name</th>
-                <td id='comparison-modal-table-td2name'>{clicked.name}</td>
-              </tr>
-              <tr id='comparison-modal-table-row-category'>
-                <td id='comparison-modal-table-td1category'>{current.category}</td>
-                <th id='comparison-modal-table-th-category' scope="row">Dept</th>
-                <td id='comparison-modal-table-td2category'>{clicked.category}</td>
-              </tr>
-              <tr id='comparison-modal-table-row-price'>
-                <td id='comparison-modal-table-td1price'>${current.price}</td>
-                <th id='comparison-modal-table-th-price' scope="row">Price</th>
-                <td id='comparison-modal-table-td2price'>${clicked.price}</td>
-              </tr>
-              <tr id='comparison-modal-table-row-rating'>
-                <td id='comparison-modal-table-td1rating'>{currentRating}</td>
-                <th id='comparison-modal-table-th-price' scope="row">Rating</th>
-                <td id='comparison-modal-table-td2rating'>{clickedRating}</td>
-              </tr>
-            </tbody>
+            {rows}
           </table>
         </div>
       </div>
