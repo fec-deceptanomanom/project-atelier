354c6ba4f79480888ced51e15e6d4bf380b19902
Logan Michalicek <logan.michalicek@gmail.com>
2021-10-06 08:29:06 -0600

    Left and right image arrows now functional

--- client/src/components/productOverview/subcomponents/ProductImage.jsx
+++ client/src/components/productOverview/subcomponents/ProductImage.jsx
@@ -9,9 +9,24 @@ class ProductImage extends React.Component {
     this.state={
       currentPhoto: props.currentStyle ? props.currentStyle.photos[0] : null,
       currentPhotoIndex: 0,
-      currentStyleId: props.currentStyle.style_id
+      currentStyleId: props.currentStyle.style_id,
+      numberOfPhotos: Object.keys(props.currentStyle.photos).length
     };
     this.setImage = this.setImage.bind(this);
+    this.changeToLeftImage = this.changeToLeftImage.bind(this);
+    this.changeToRightImage = this.changeToRightImage.bind(this);
+  }
+
+  changeToLeftImage() {
+    if (this.state.currentPhotoIndex > 0) {
+      this.setImage(this.state.currentPhotoIndex - 1);
+    }
+  }
+
+  changeToRightImage() {
+    if (this.state.currentPhotoIndex < (this.state.numberOfPhotos - 1)) {
+      this.setImage(this.state.currentPhotoIndex + 1);
+    }
   }
 
   setImage(idx) {
@@ -33,6 +48,7 @@ class ProductImage extends React.Component {
   }
 
   render() {
+    console.log(this.state.numberOfPhotos);
     if (this.props.currentStyle) {
       let imageElement;
       if (this.state.currentPhoto && this.state.currentPhoto.url !== null) {
@@ -46,8 +62,24 @@ class ProductImage extends React.Component {
           onClick={this.props.onClick}
           id={'product-overview-image-space'}
         >
+          <button
+            id={'image-button-left'}
+            className={CSSCommon['img-shift-left-arrow']}
+            onClick={this.changeToLeftImage}
+          >
+            <i className="fas fa-chevron-left"></i>
+          </button>
+
           <ImageCarousel images={this.props.currentStyle.photos} setImage={this.setImage} currentStyleId={this.props.currentStyle.style_id}/>
           {imageElement}
+
+          <button
+            id={'image-button-right'}
+            className={CSSCommon['img-shift-right-arrow']}
+            onClick={this.changeToRightImage}
+          >
+            <i className="fas fa-chevron-right"></i>
+          </button>
         </div>
       );
     } else {
