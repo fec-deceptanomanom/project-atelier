5b70bd67869fb5c6c6d06d15713736670a499b08
Sarah Cerezo <cerezo.sarah@gmail.com>
2021-10-06 23:17:29 -0400

    Misc optimization tweaks and SVG files

--- client/src/components/questionsAndAnswers/subcomponents/PhotoZoom.jsx
+++ client/src/components/questionsAndAnswers/subcomponents/PhotoZoom.jsx
@@ -6,7 +6,7 @@ const PhotoZoom = (props) => {
     <div id={'zoomed-in-photo-div' + props.answerID} className={CSSStyle.modal}>
       <div id="zoomed-in-answer-photo" className={[CSSStyle['modal-content'], CSSStyle['photo-zoom']].join(' ')}>
        <span id="close-photo-zoom-span" className={CSSStyle.close} aria-label="Close" onClick={props.closeZoom}><i id="close-photo-zoom" className="far fa-times-circle"></i></span>
-      <img id="answer-photo-zoom" className={CSSStyle['zoom-photo']} src={props.photo}></img>
+      <img id="answer-photo-zoom" className={CSSStyle['zoom-photo']} src={props.photo} alt="Zoomed in Photo"></img>
       </div>
     </div>
   )
