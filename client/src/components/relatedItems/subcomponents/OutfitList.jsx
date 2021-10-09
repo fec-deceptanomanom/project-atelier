dc5455c81fc9d83a7b293aec8d8b351372e751e1
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-10-09 09:58:52 -0400

    outfit list API call returning.

--- client/src/components/relatedItems/subcomponents/OutfitList.jsx
+++ client/src/components/relatedItems/subcomponents/OutfitList.jsx
@@ -3,17 +3,60 @@ import CSSLight from './../relatedItemsLight.module.css';
 
 import OutfitCard from './OutfitCard.jsx';
 import AddToOutfit from './AddToOutfit.jsx';
+import axios from 'axios';
+import $ from 'jquery';
+import URL_BASE from '../../../../../.secretURL.json';
 
 class Outfitlist extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      outfits: [1, 1, 1]
+    }
+  }
+  componentDidMount() {
+    //check local storage
+    let storage = JSON.parse(localStorage.getItem('0, 1, 2'));
+    const urlId = window.location.href.split('/p/')[1].replace('/', '');
+    let ids = [];
+    console.log('storage', storage);
+    // if any outfits have a length of 5
+    storage.forEach( (item) => {
+      console.log('item', typeof item)
+      if (item !== 1) {
+        ids.push(item);
+      }
+    });
+    console.log('IDS', ids);
 
+    if (ids.length > 0) {
+      Promise.all(
+        ids.map(id => {
+          return new Promise((resolve, reject) => {
+            axios.get(`/productInfo/${id}`)
+              .then(response => {
+                resolve(response.data);
+              })
+              .catch(err => {
+                console.error(err);
+              })
+            })
+          })
+        )
+        .then(results => {
+          console.log('results', results);
+          this.setState({
+            outfits: results
+          })
+        })
+        .catch(err => {
+          console.error(err);
+        })
     }
   }
   render() {
-    let outfits = this.props.outfits || ['', '', ''];
-
+    let outfits = this.state.outfits;
+    console.log('outfits', outfits)
     outfits = outfits.map( (item, i) => {
       return (
         <div id='outfit-card-container' className={CSSLight.card} key={i}>
