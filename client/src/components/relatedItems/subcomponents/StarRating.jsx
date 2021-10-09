a93b0adb05ae6796f42c4611c511c1fa722e0e2d
ryhorowitz <ryan.w.horowitz@gmail.com>
2021-10-09 00:13:04 -0400

    init

--- client/src/components/relatedItems/subcomponents/StarRating.jsx
+++ client/src/components/relatedItems/subcomponents/StarRating.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// import CSSCommon from './client/src/components/productOverview/styles/productOverview.module.css';
-import CSSLight from '.././relatedItemsLight.module.css';
+.import CSSLight from '.././relatedItemsLight.module.css';
 import CSSDark from '.././relatedItemsDark.module.css';
 const getRoundedRating = function(ratings) {
   let ratingSum = 0;
